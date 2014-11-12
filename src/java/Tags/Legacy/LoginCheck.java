f745dcf53cdc0984f924583b170ca63abe1a77bf
perHub <perHub@github>
2014-11-11 23:21:28 -0300

    Still working with the JSF tag...

--- src/java/Tags/Legacy/LoginCheck.java
+++ src/java/Tags/Legacy/LoginCheck.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Tags;
+package Tags.Legacy;
 
 import Exceptions.AdminClienteException;
 import Exceptions.loginException;
